7fbfaedbe8bdf01770f5e3d1d3ac1ed21473107d
princebhayani <princebhayani192003@gmail.com>
2025-02-24 18:02:15 +0530

    add meals page layout

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,20 @@
 import Link from "next/link"
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage(){
     return(
-        <main>
-            <h1>
-                Meal Page
-            </h1>
-            <p>
-            <Link href="meals/meal1">Meal 1</Link>
+        <>
+        <header className={classes.header}>
+            <h1>Delicious meals, created{' '}<span className={classes.highlight}>by you</span></h1>
+            <p>Choose your favorite recipe ans cook it yourself. It is easy and fun!</p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">Share Your Favorite Recipe</Link>
             </p>
-            <p><Link href="meals/share">share</Link></p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]} />
         </main>
+        </>
     )
 }
