ee248d2fd1897802f25e983c8ebc67b73db3023f
princebhayani <princebhayani192003@gmail.com>
2025-02-25 05:42:13 +0530

    add not found page and error page

---
+++ app/meals/error.js
@@ -0,0 +1,7 @@
+"use client"
+export default function Error({error}){
+    return <main className="error">
+        <h1>An Error occurred!</h1>
+        <p>Failed to fetch meal data. Please try again later.</p>
+    </main>
+}
