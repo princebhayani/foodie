5ff60b3d8108a73bc0f2f30c8bca66a4e600ec0a
princebhayani <princebhayani192003@gmail.com>
2025-03-02 04:56:20 +0530

    managing form submission with action state

---
+++ components/meals/meals-form-submit.js
@@ -0,0 +1,8 @@
+"use client";
+import { useActionState } from "react";
+
+export default function MealsFormSubmit(){
+    const {pending} = useActionState();
+
+    return <button disabled={pending}>{pending ? 'Submitting...' : 'Share Meal'}</button>
+}
